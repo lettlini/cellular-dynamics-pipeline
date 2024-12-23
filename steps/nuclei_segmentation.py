6a9f7258a0c7dcee7a87d363c7c54ff931c5af51
Kolya Lettl <klettl@posteo.com>
2024-12-23 16:27:37 +0100

    refactored function 'get_disconnected' such that it can be easily imported elsewhere

--- steps/nuclei_segmentation.py
+++ steps/nuclei_segmentation.py
@@ -1,5 +1,4 @@
 import os
-
 import pickle
 from argparse import ArgumentParser
 
@@ -16,6 +15,25 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = (
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 
+def get_disconnected(timage: np.array) -> np.array:
+    """Disconnects touching regions with different labels (stardist)"""
+
+    dmask = np.zeros_like(timage)
+
+    # get list of labels
+    lls = np.unique(timage)
+    lls = np.setdiff1d(lls, (0,))
+
+    for current_label in lls:
+        current_small_image = (timage == current_label).astype(np.uint8)
+        current_small_image = cv2.erode(
+            current_small_image, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        )
+        dmask += current_small_image
+
+    return ~(dmask.astype(bool))
+
+
 class StarDistSegmentationTransform(BaseDataSetTransformation):
     def __init__(
         self,
@@ -26,25 +44,6 @@ class StarDistSegmentationTransform(BaseDataSetTransformation):
 
         super().__init__()
 
-    @staticmethod
-    def get_disconnected(timage: np.array) -> np.array:
-        """Disconnects touching regions with different labels (stardist)"""
-
-        dmask = np.zeros_like(timage)
-
-        # get list of labels
-        lls = np.unique(timage)
-        lls = np.setdiff1d(lls, (0,))
-
-        for current_label in lls:
-            current_small_image = (timage == current_label).astype(np.uint8)
-            current_small_image = cv2.erode(
-                current_small_image, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-            )
-            dmask += current_small_image
-
-        return ~(dmask.astype(bool))
-
     def _transform_single_entry(self, entry: BaseDataSetEntry) -> BaseDataSetEntry:
 
         image = entry.data
@@ -52,7 +51,7 @@ class StarDistSegmentationTransform(BaseDataSetTransformation):
         labels, info = self.stardist_model.predict_instances(image)
 
         # We need to disconnect touching labels:
-        dmask = StarDistSegmentationTransform.get_disconnected(labels)
+        dmask = get_disconnected(labels)
         labels[dmask == 1] = 0
 
         if self.probability_threshold is not None:
@@ -120,7 +119,7 @@ if __name__ == "__main__":
         "--min_nucleus_area_mumsq",
         required=True,
         type=float,
-        help="Nuclei smaller than min_nucleus_area_mumsq will be removed",
+        help="Nuclei smaller than 'min_nucleus_area_mumsq' will be removed",
     )
 
     args = parser.parse_args()
