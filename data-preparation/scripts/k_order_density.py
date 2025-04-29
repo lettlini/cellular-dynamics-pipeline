8c558bad0c4de3a2552c61205b79ba77b9e83d91
Kolya Lettl <klettl@posteo.com>
2025-04-29 17:27:54 +0200

    feature: add different k-scalers

--- data-preparation/scripts/k_order_density.py
+++ data-preparation/scripts/k_order_density.py
@@ -34,26 +34,68 @@ def get_k_fold_neighbors(G, node, k):
 
 
 class AnnotateKFoldNeighborhoodDensity(BaseDataSetTransformation):
-    def __init__(self, k):
+    def __init__(self, k, prefix: str):
         self._k = k
+        self._prefix = prefix
+
         super().__init__()
+
+    def _k_scaling(self, k: int) -> float:
+        raise NotImplementedError("k scaling function has not yet been implemented")
 
     def _transform_single_entry(self, entry, dataset_properties):
         # here we want to annotate each node with its k-order neighborhood density
 
-        graph: nx.Graph = entry.data.copy()
+        graph: nx.Graph = entry.data
 
         for node in graph.nodes:
             areas = []
+            weights = []
 
             for ck in range(self._k + 1):
+                k_scaler: float = self._k_scaling(ck)
+
                 for cnb in get_k_fold_neighbors(graph, node, ck):
                     areas.append(graph.nodes[cnb]["cell_area_mum_squared"])
-                    graph.nodes[node][f"{ck}-order_local_density_per_mum_squared"] = (
-                        1 / np.mean(np.array(areas))
-                    )
+                    weights.append(k_scaler)
+
+                graph.nodes[node][
+                    f"{self._prefix}-{ck}-order_local_density_per_mum_squared"
+                ] = 1 / np.average(np.array(areas), weights=np.array(weights))
 
         return BaseDataSetEntry(identifier=entry.identifier, data=graph)
+
+
+class StaticKFoldNeighborhoodDensity(AnnotateKFoldNeighborhoodDensity):
+    def __init__(self, k):
+        super().__init__(k, "static")
+
+    def _k_scaling(self, k):
+        return 1.0
+
+
+class InverseLinearKFoldNeighborhoodDensity(AnnotateKFoldNeighborhoodDensity):
+    def __init__(self, k):
+        super().__init__(k, "inverse_linear")
+
+    def _k_scaling(self, k):
+        return 1.0 / max(1, k)
+
+
+class InverseQuadraticKFoldNeighborhoodDensity(AnnotateKFoldNeighborhoodDensity):
+    def __init__(self, k):
+        super().__init__(k, "inverse_quadratic")
+
+    def _k_scaling(self, k):
+        return 1.0 / (max(k, 1) ** 2)
+
+
+class GaussianDecayKFoldNeighborhoodDensity(AnnotateKFoldNeighborhoodDensity):
+    def __init__(self, k):
+        super().__init__(k, "gaussian_decay")
+
+    def _k_scaling(self, k):
+        return np.exp(-k * k)
 
 
 if __name__ == "__main__":
@@ -72,6 +114,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     x = BaseDataSet.from_pickle(args.infile)
-    x = AnnotateKFoldNeighborhoodDensity(k=7)(x, cpus=args.cpus)
+
+    x = StaticKFoldNeighborhoodDensity(k=7)(x, cpus=args.cpus)
+    x = InverseLinearKFoldNeighborhoodDensity(k=7)(x, cpus=args.cpus)
+    x = InverseQuadraticKFoldNeighborhoodDensity(k=7)(x, cpus=args.cpus)
+    x = GaussianDecayKFoldNeighborhoodDensity(k=7)(x, cpus=args.cpus)
 
     x.to_pickle(args.outfile)
