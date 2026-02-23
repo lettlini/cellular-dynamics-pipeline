a536cffac0f20a497e4b7b72a6729f1831540a41
Leon Hillmann <l.hillmann@tue.nl>
2026-02-23 13:52:26 +0100

    feat: Umbrella specific configuration

---
+++ launch-umbrella.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=nextflow_pipeline   # Job name
+#SBATCH --output=/home/20235660/nextflow-logs/nextflow_%j.out       # Standard output log (%j will be replaced with the job ID)
+#SBATCH --error=/home/20235660/nextflow-logs/nextflow_%j.err        # Standard error log (%j will be replaced with the job ID)
+#SBATCH --ntasks=1                     # Number of tasks (we're running a single task, Nextflow will handle the rest)
+#SBATCH --cpus-per-task=1             # Number of CPU cores per task
+#SBATCH --mem=8G                     # Memory allocation per task (adjust as needed)
+#SBATCH --time=4-00:00:00
+#SBATCH --partition=phys.default.q           # Partition to submit to (adjust if needed)
+
+OUTPUT_DIR=/home/20235660/nextflow/nextflow-reports/${SLURM_JOB_ID}/
+mkdir -p $OUTPUT_DIR  # Create the output directory if it doesn't exist
+
+# Load Nextflow module
+module load Java
+module load libglvnd
+module load CUDA/12.8
+
+# Load Mamba module & make sure conda and mamba are on the path
+# module load Mamba
+# source /software/easybuild/el8/amd_zen1/all/Mamba/23.1.0-4/etc/profile.d/conda.sh
+
+CONFIG_ID=$1
+
+nextflow run ./pipeline-umbrella.nf \
+    -c ./dataset_configs/$CONFIG_ID.config \
+    -profile umbrella \
+    -with-report ${OUTPUT_DIR}/report.html \
+    -with-timeline ${OUTPUT_DIR}/timeline.html \
+    -with-trace ${OUTPUT_DIR}/trace.txt \
+    "${@:2}"
